50382cf1fb10ce65eeb29cee3b3b2814eb67a8ae
vizzie <ed@vizzie.com>
2019-10-27 17:12:15 -0400

    c++ doesnt like getting value from menuconfig definition

--- main/main.cpp
+++ main/main.cpp
@@ -17,7 +17,7 @@
 /* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define BLINK_GPIO GPIO_NUM_16
 
 extern "C"
 {
